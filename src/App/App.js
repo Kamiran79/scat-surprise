c11447a6bd3dac29a9f67afb559e1f06b381ecd8
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-08-29 13:03:22 -0500

    navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,81 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/pages/Auth/Auth';
+import EditBirb from '../components/pages/EditBirb/EditBirb';
+import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import NewBirb from '../components/pages/NewBirb/NewBirb';
+import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
+
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <h2>Scat Surprise</h2>
-        <button className="btn btn-info"><i className="fas fa-surprise"></i> I am a button <i className="fas fa-surprise"></i></button>
+        {/* <h2>Scat Surprise</h2>
+        <button className="btn btn-info"><i className="fas fa-surprise"></i> I am a button <i className="fas fa-surprise"></i></button> */}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed} />
+            <div className="container">
+              <Switch>
+                <PrivateRoute path="/home" component={Home} authed={authed} />
+                <PrivateRoute path="/new" component={NewBirb} authed={authed} />
+                <PrivateRoute path="/edit/:birbId" component={EditBirb} authed={authed} />
+                <PrivateRoute path="/birbs/:birbId" component={SingleBirb} authed={authed} />
+                <PublicRoute path="/auth" component={Auth} authed={authed} />
+                <Redirect from="*" to="/home"/>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
