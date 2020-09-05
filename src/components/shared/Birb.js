eeccac2bc3790dbc83038526a48f6ae4b6c508b4
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-09-05 10:16:34 -0500

    delete a birb from home page

--- src/components/shared/Birb.js
+++ src/components/shared/Birb.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 import birbShape from '../../helpers/propz/birbShape';
 
+import './Birb.scss';
+
 class Birb extends React.Component {
   static propTypes = {
     birb: birbShape.birbShape,
+    deleteBirb: PropTypes.func.isRequired,
   }
+
+  deletebirbEvent = (e) => {
+    e.preventDefault();
+    const { deleteBirb, birb } = this.props;
+    deleteBirb(birb.id);
+  };
 
   render() {
     const { birb } = this.props;
@@ -18,17 +27,27 @@ class Birb extends React.Component {
     // moment(birb.seenAt).format('MM-DD-YYYY')
 
     const singleBirbLink = `/birbs/${birb.id}`;
+    const editLink = `/edit/${birb.id}`;
     return (
-      <div className="card text-center" style={{ background: `"${birb.color}"` }}>
-        <div className="card-header"><h5>{birb.type}</h5></div>
-        <div className="card-body">
-          <p className="card-title">{birb.color}</p>
-          <p className="card-text">{birb.size}</p>
-          <p className="card-text">{filterDate}</p>
-          <Link to={singleBirbLink} className="btn btn-warning mr-1"><i class="far fa-eye"></i></Link>
-          <button className="btn btn-danger" onClick={this.deletebirbEvent}>Delete birb</button>
+      <div className="col-4 mb-3">
+        <div className="card text-center boarder-0" style={{ background: `"${birb.color}"` }}>
+          <div className="card-header">
+            <h5>{birb.type}</h5>
+            <Link to={editLink} className="btn btn-success mr-1 float-left"><i className="fas fa-pencil-alt"></i></Link>
+            <button className="btn btn-danger float-right" onClick={this.deletebirbEvent}><i className="fas fa-trash-alt "></i></button>
+          </div>
+          <div className="card-body">
+            <p className="card-title">{birb.color}</p>
+            <p className="card-text">{birb.size}</p>
+            <p className="card-text">{filterDate}</p>
+            <p className="card-text">{birb.notes}</p>
+            <Link to={singleBirbLink} className="btn btn-warning mr-1"><i class="far fa-eye"></i></Link>
+
+          </div>
+          <div className="card-footer text-muted">
+            Updated: {moment(birb.seenAt).fromNow()}
+          </div>
         </div>
-    <div className="card-footer text-muted">{birb.note}</div>
       </div>
     );
   }
